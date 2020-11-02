f5ce6f9ad88fb057a8ff9b8b94e556461351c635
1ng-HeberH <ing.heberh@gmail.com>
2020-11-02 11:51:22 -0600

    Code corrections from the code review

--- Reservaciones/Screens/Login.js
+++ Reservaciones/Screens/Login.js
@@ -1,118 +1,133 @@
-
-import React, { useState, useEffect } from 'react';
-import styleLogin from '../Styles/LoginStyle';
-import { GoogleSocialButton } from "react-native-social-buttons";
-import { View, Text, TextInput, TouchableOpacity, Linking, Button} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
+import React, {useState, useEffect} from 'react';
+import {
+   View,
+   Text,
+   TextInput,
+   TouchableOpacity,
+   Linking,
+   Button,
+} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEye} from '@fortawesome/free-solid-svg-icons';
 import auth from '@react-native-firebase/auth';
-import { GoogleSignin } from '@react-native-community/google-signin';
+import {GoogleSignin} from '@react-native-community/google-signin';
+import styleLogin from '../Styles/LoginStyle';
+import {GoogleSocialButton} from 'react-native-social-buttons';
 
 GoogleSignin.configure({
-  webClientId: '346661789891-e6taibn68bvqogs5h93gs9bgdbt3utlp.apps.googleusercontent.com',
+  webClientId:
+    '346661789891-e6taibn68bvqogs5h93gs9bgdbt3utlp.apps.googleusercontent.com',
 });
 
 async function onGoogleButtonPress() {
-   // Get the users ID token
-   const { idToken } = await GoogleSignin.signIn();
- 
-   // Create a Google credential with the token
-   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
- 
-   // Sign-in the user with the credential
-   return auth().signInWithCredential(googleCredential);
- }
+  const {idToken} = await GoogleSignin.signIn();
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  return auth().signInWithCredential(googleCredential);
+}
 
 function LoginApp() {
-   // Set an initializing state whilst Firebase connects
-   const [initializing, setInitializing] = useState(true);
-   const [user, setUser] = useState();
- 
-   // Handle user state changes
-   function onAuthStateChanged(user) {
-     setUser(user);
-     if (initializing) setInitializing(false);
-   }
- 
-   useEffect(() => {
-     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-     return subscriber; // unsubscribe on unmount
-   }, []);
- 
-   if (initializing) return null;
- 
-   if (!user) {
-     return (
-       <View>
-         <Text>Login principal</Text>
-       </View>
-     );
-   }
- 
-   return (
-     <View>
-       <Text>Bienvenido {user.email}</Text>
-     </View>
-   );
- }
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-const Login = () => {
-   const [isHidden, setIsHidden] = useState(true);
-
-
-  cerrarsesion = () => {
-   auth()
-   .signOut()
-   .then(() => console.log('User signed out!'));
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
   }
 
-   return (
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
-      <View style={styleLogin.Father}>
-        
-        <View>
-            <Text style={styleLogin.TextTittle}>Log In</Text>
-            <Text style={styleLogin.InputTittle}>Email * </Text>   
-            <TextInput style={styleLogin.InputText}/>
-            <Text style={styleLogin.InputTittle}>Password *</Text>
-            <View style={styleLogin.eyeContainer}>  
-               <View>
-                  <TextInput 
-                     secureTextEntry={isHidden}
-                     style={styleLogin.InputText} 
-                  />
-                  <TouchableOpacity
-                  style={styleLogin.eyeIcon}
-                  onPressIn={() => setIsHidden(!isHidden)}>
-                     <FontAwesomeIcon icon={ faEye } />
-                  </TouchableOpacity>
-               </View>
-            </View>
-        </View>
-         <View style={styleLogin.ButomsArea}>
-            <TouchableOpacity
-               style={styleLogin.loginScreenButton}
-               underlayColor='#fff'>
-               <Text style={styleLogin.loginText}>Login</Text>
-         </TouchableOpacity>
-            <Text style={{textAlign: 'center', color:'#B6B7BA', margin: 10}}>or</Text>
-            <TouchableOpacity
-            >
-               <GoogleSocialButton
-               onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))} />
-            </TouchableOpacity>
-            <Text style={{marginTop: 20}}>
-               <Text style={{color: '#B6B7BA'}}>Don´t have an account? </Text>
-               <Text style={{color: '#5974F5', textDecorationLine: 'underline'}} 
-		         onPress={ ()=> Linking.openURL('https://www.google.com/') }>Sign Up</Text>    
-            </Text>
-            <LoginApp/>
-            <Button
-            title= 'Cerrar sesion'
-            onPress = {() => cerrarsesion()}/>
-         </View>
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login principal</Text>
       </View>
-   );
+    );
+  }
+
+  return (
+    <View>
+      <Text>Bienvenido {user.email}</Text>
+    </View>
+  );
 }
+
+const Login = () => {
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+  const customStyleFocus = isActive
+    ? styleLogin.textInputFocus
+    : styleLogin.InputText;
+  const signOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
+  return (
+    <View style={styleLogin.Father}>
+      <View>
+        <Text style={styleLogin.TextTittle}>Log In</Text>
+        <Text style={styleLogin.InputTittle}>Email * </Text>
+        <TextInput 
+            onFocus={() => setIsActive(true)}
+            onBlur={() => setIsActive(false)}
+            style={customStyleFocus} 
+         />
+        <Text style={styleLogin.InputTittle}>Password *</Text>
+        <View style={styleLogin.eyeContainer}>
+          <View>
+            <TextInput
+              secureTextEntry={isPasswordHidden}
+              onFocus={() => setIsActive(true)}
+              onBlur={() => setIsActive(false)}
+              style={customStyleFocus}
+            />
+            <TouchableOpacity
+              style={styleLogin.eyeIcon}
+              onPressIn={() => setIsPasswordHidden(!isPasswordHidden)}>
+              <FontAwesomeIcon icon={faEye} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View style={styleLogin.ButomsArea}>
+        <TouchableOpacity
+          style={styleLogin.loginScreenButton}
+          underlayColor="#fff">
+          <Text style={styleLogin.loginText}>Login</Text>
+        </TouchableOpacity>
+        <Text style={{textAlign: 'center', color: '#B6B7BA', margin: 10}}>
+          or
+        </Text>
+        <TouchableOpacity>
+          <GoogleSocialButton
+            onPress={() =>
+              onGoogleButtonPress().then(() =>
+                console.log('Signed in with Google!'),
+              )
+            }
+          />
+        </TouchableOpacity>
+        <Text style={{marginTop: 20}}>
+          <Text style={{color: '#B6B7BA'}}>Don´t have an account? </Text>
+          <Text
+            style={{color: '#5974F5', textDecorationLine: 'underline'}}
+            onPress={() => Linking.openURL('https://www.google.com/')}>
+            Sign Up
+          </Text>
+        </Text>
+        <LoginApp />
+        <Button title="Sign Out" onPress={() => signOut()} />
+      </View>
+    </View>
+  );
+};
 
 export default Login;
--- Reservaciones/Styles/LoginStyle.js
+++ Reservaciones/Styles/LoginStyle.js
@@ -1,102 +1,111 @@
-import {  StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const LoginStyle = StyleSheet.create({
-    Father:{
-        flex:1,
-        padding: 15,
-        width: '100%',
-    },
-    TextTittle:{
-        color: '#5C6EF8',
-        fontSize: 22,
-        fontWeight: 'bold',
-        textAlign: 'left',
-        marginTop: 25,
-        marginBottom: 25, 
-    },
-    InputTittle: {
-        marginBottom: 10,
-        color: '#B6B7BA'
+  Father: {
+    flex: 1,
+    padding: 15,
+    width: '100%',
+  },
+  TextTittle: {
+    color: '#5C6EF8',
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginTop: 25,
+    marginBottom: 25,
+  },
+  InputTittle: {
+    marginBottom: 10,
+    color: '#B6B7BA',
+  },
+  InputText: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexWrap: 'wrap',
+    height: 48,
+    justifyContent: 'center',
+    marginBottom: 15,
+    width: 400,
+    color: 'black',
+  },
+  textInputFocus: {
+    borderColor: 'blue',
+    borderWidth: 1,
+    flexWrap: 'wrap',
+    height: 48,
+    justifyContent: 'center',
+    marginBottom: 15,
+    width: 400,
+    color: 'black',
+  },
+  ShowPass: {
+    alignSelf: 'flex-end',
+    alignContent: 'flex-end',
+    marginRight: 5,
+  },
+  ButomsArea: {
+    alignItems: 'center',
+    color: 'white',
+    flex: 1,
+    marginTop: 40,
+  },
+  GoogleLoginButton: {
+    backgroundColor: '#5C6EF8',
+    borderRadius: 10,
+    flexWrap: 'wrap',
+    height: 25,
+    justifyContent: 'center',
+    margin: 10,
+    padding: 20,
+  },
+  loginScreenButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#B6B7BA',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    width: '90%',
+    height: 47,
+  },
+  loginText: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontWeight: 'bold',
+  },
+  searchSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  input: {
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    backgroundColor: '#fff',
+    color: '#424242',
+  },
+  eyeIcon: {
+    paddingTop: 17,
+    paddingRight: 10,
+    position: 'absolute',
+    alignSelf: 'flex-end',
+  },
+  eyeContainer: {
+    flexDirection: 'row',
+    // justifyContent: 'center'
+  },
+});
 
-    },
-    InputText: {
-        borderColor: 'gray', 
-        borderWidth: 1,
-        flexWrap: 'wrap', 
-        height: 48,
-        justifyContent: 'center',
-        marginBottom: 15,
-        width: 380 ,
-        color: 'black',
-    },
-    ShowPass:{
-        alignSelf: 'flex-end',
-        alignContent: 'flex-end',
-        marginRight: 5,
-    },
-    ButomsArea:{
-        alignItems: 'center',
-        color: 'white',
-        flex:1,
-        marginTop: 40,
-    },
-    GoogleLoginButton: {
-        backgroundColor: '#5C6EF8',
-        borderRadius: 10,
-        flexWrap: 'wrap', 
-        height: 25,
-        justifyContent: 'center',
-        margin: 10,
-        padding: 20,
-    },
-    loginScreenButton:{
-        marginRight:40,
-        marginLeft:40,
-       marginTop:10,
-        paddingTop:10,
-        paddingBottom:10,
-        backgroundColor:'#B6B7BA',
-        borderRadius:10,
-        borderWidth: 1,
-        borderColor: '#fff',
-        width: '90%',
-        height: 47
-      },
-      loginText:{
-          color:'#fff',
-          textAlign:'center',
-          paddingLeft : 10,
-          paddingRight : 10,
-          fontWeight: 'bold'
-      },
-      searchSection: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    },
-    searchIcon: {
-        padding: 10,
-    },
-    input: {
-        paddingTop: 10,
-        paddingRight: 10,
-        paddingBottom: 10,
-        paddingLeft: 0,
-        backgroundColor: '#fff',
-        color: '#424242',
-    },
-    eyeIcon: {
-        paddingTop: 17,
-        paddingRight: 10,
-        position: 'absolute',
-        alignSelf: 'flex-end',
-    },
-    eyeContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center'
-    }
-  });
-
-  export default LoginStyle;
+export default LoginStyle;
